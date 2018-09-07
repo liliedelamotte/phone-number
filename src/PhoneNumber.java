ddf8c11bf1f7accf8345709bc5bb8056f6d70375
Lilie de la Motte <liliedelamotte@gmail.com>
2018-09-07 10:46:03 -0700

    add JavaDoc template to the class and all methods

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,40 +1,91 @@
+// ldelamotte17@georgefox.edu
 
-public class PhoneNumber {
+/**
+ * 
+ *
+ */
+public class PhoneNumber implements Comparable {
+    
     
     private String _areaCode;
     private String _prefix;
     private String _lineNumber;
 
+    
+    /**
+     * 
+     * @param areaCode
+     * @param prefix
+     * @param lineNumber
+     */
     public PhoneNumber(String areaCode, String prefix, String lineNumber) {
         
     }
     
+    
+    /**
+     * 
+     * @return
+     */
     public String getAreaCode() {
         return _areaCode;
     }
     
+    
+    /**
+     * 
+     * @return
+     */
     public String getPrefix() {
         return _prefix;
     }
     
+    
+    /**
+     * 
+     * @return
+     */
     public String getLineNumber() {
         return _lineNumber;
     }
     
+    
+    /**
+     * 
+     * @return
+     */
     public String getDigits() {
         return "";
     }
     
+    
+    /**
+     * 
+     */
     public int compareTo(Object PhoneNumber) {
         return 1;
     }
     
+    
+    /**
+     * 
+     * @param areaCode
+     * @param prefix
+     * @param lineNumber
+     * @return
+     */
     public static boolean isValidPhoneNumber(String areaCode, String prefix, String lineNumber) {
         return true;
     }
     
-    public static PhoneNumber parsePhoneNumber(String phoneNumber) {
-        
+    
+    /**
+     * 
+     * @param phoneNumber
+     * @return
+     */
+    public static PhoneNumber parsePhoneNumber(PhoneNumber phoneNumber) {
+        return phoneNumber;
     }
     
 }
