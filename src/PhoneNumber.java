9cdcf972873df1528a136d0eb18dcee00cd39b59
Lilie de la Motte <liliedelamotte@gmail.com>
2018-09-07 10:42:44 -0700

    create empty methods

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,4 +1,40 @@
 
 public class PhoneNumber {
+    
+    private String _areaCode;
+    private String _prefix;
+    private String _lineNumber;
 
+    public PhoneNumber(String areaCode, String prefix, String lineNumber) {
+        
+    }
+    
+    public String getAreaCode() {
+        return _areaCode;
+    }
+    
+    public String getPrefix() {
+        return _prefix;
+    }
+    
+    public String getLineNumber() {
+        return _lineNumber;
+    }
+    
+    public String getDigits() {
+        return "";
+    }
+    
+    public int compareTo(Object PhoneNumber) {
+        return 1;
+    }
+    
+    public static boolean isValidPhoneNumber(String areaCode, String prefix, String lineNumber) {
+        return true;
+    }
+    
+    public static PhoneNumber parsePhoneNumber(String phoneNumber) {
+        
+    }
+    
 }
